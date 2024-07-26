084e8313bb5dd70e57dbe842125a79d27eb3b79c
nickpastoria <nickpastoria@gmail.com>
2024-07-25 20:29:10 -0700

    The nightmare is coming to an end. I got snippets working. I'm crying

--- lua/nickpasta/plugins/LuaSnip.lua
+++ lua/nickpasta/plugins/LuaSnip.lua
@@ -1,8 +1,11 @@
-require({
+return {
 	"L3MON4D3/LuaSnip",
 	-- follow latest release.
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	build = "make install_jsregexp",
-	dependencies = { "rafamadriz/friendly-snippets" },
-})
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"saadparwaiz1/cmp_luasnip",
+	},
+}
