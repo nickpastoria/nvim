bdeb48598fdd7791d1b7bad1007e8ea064c24081
nickpastoria <nickpastoria@gmail.com>
2024-08-09 12:21:57 -0700

    added hotkey for tabbing through vim winodws and template code for copy, paste of file path

--- lua/nickpasta/commands.lua
+++ lua/nickpasta/commands.lua
@@ -5,3 +5,4 @@ end
 -- Quick Settings
 command("Commands", "edit ~/.config/nvim/lua/nickpasta/commands.lua")
 command("Settings", "edit ~/.config/nvim/lua/nickpasta/settings.lua")
+command("Maps", "edit ~/.config/nvim/lua/nickpasta/maps.lua")
--- lua/nickpasta/maps.lua
+++ lua/nickpasta/maps.lua
@@ -8,7 +8,7 @@ end
 map("n", "<leader>w", "<CMD>update<CR>")
 
 -- Quit
-map("n", "<leader>q", "<CMD>q<CR>")
+map("n", "<leader>q", "<CMD>wq!<CR>")
 
 -- Exit insert mode
 map("i", "jk", "<ESC>")
@@ -16,6 +16,9 @@ map("i", "jk", "<ESC>")
 -- NeoTree
 map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
 map("n", "<leader>r", "<CMD>Neotree focus<CR>")
+
+-- Special Functions
+map("n", "<leader>f", "<CMD>:let @+ = expand('%')<CR>")
 
 -- New Windows
 map("n", "<leader>o", "<CMD>vsplit<CR>")
@@ -26,6 +29,7 @@ map("n", "<C-h>", "<C-w>h")
 map("n", "<C-l>", "<C-w>l")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-j>", "<C-w>j")
+map("n", "<leader>t", "<C-w>w")
 
 -- Resize Windows
 map("n", "<C-Left>", "<C-w><")
@@ -34,4 +38,4 @@ map("n", "<C-Up>", "<C-w>+")
 map("n", "<C-Down>", "<C-w>-")
 
 -- Terminal Commands
-map("t", "<C-w>h", "<C-\\><C-n><C-w>h", { silent = true })
+map("t", "<C-w>h", "<C-\\><C-n><C-w>h")
